8a4615fc50eaf66f2b01df7f4321baf02de68c31
Anna Ingels <annaingels131@yahoo.com>
2018-09-23 16:11:38 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+    vlicenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
+    verticalCentered: false,
+    navigation: true,
+    anchors: ['Home', 'Projects', 'About'],
+    sectionsColor: ['#ffb6b9', '#59C0BF', '#B9DED7'],
+});
+
+//methods
+fullpage_api.setAllowScrolling(true);
